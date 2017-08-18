9b8c9acdec21008438ab82b8788f926f166427f5
Nwke <adslk131007@mail.ru>
2017-08-18 15:13:16 +0300

    Add files via upload

---
+++ Accordion-menu-master/script.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Denis on 29.07.2017.
+ */
+
+
+
+function toggleMenu(event) {
+	if (this.classList.contains('show')) {
+		this.classList.remove('show');
+		this.classList.add('hide');
+	} else {
+		this.classList.add('show');
+		this.classList.remove('hide');
+	}
+}
+
+function openLink(event) {
+	event.preventDefault();
+	event.stopPropagation();
+}
+
+function init(node) {
+	node.addEventListener('click', toggleMenu);
+}
+
+function initLink(node) {
+	if (node.dataset.toggle) {
+		return;
+	}
+	node.addEventListener('click', openLink, true);
+}
+
+Array
+.from(document.querySelectorAll('.dropdown') )
+.forEach(init);
+
+Array
+.from(document.querySelectorAll('a'))
+.forEach(initLink);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
