e95cfab3a41672fd36dfd14089ac66a0111a4e40
Nwke <adslk131007@mail.ru>
2017-08-18 21:01:07 +0300

    Add files via upload

--- Draftsman/script.js
+++ Draftsman/script.js
@@ -2,14 +2,25 @@ let canvas = document.querySelector('#draw');
 let ctx  = canvas.getContext('2d');
 let lineWidth = 100;
 let currentHue = 0;
-let paint = false;
 
 let opts = {
-	radius: 100,
+	lineWidth: 100,
 	color: 'hsl(hue, 100%, 50%)'
 };
 
 window.addEventListener('resize', changeCanvasSize);
+
+canvas.addEventListener('mousedown', () => {
+	canvas.addEventListener('mousemove', startDraw)
+});
+
+canvas.addEventListener('mouseup', () => {
+	canvas.removeEventListener('mousemove', startDraw);
+});
+
+canvas.addEventListener('mouseleave', () => {
+	canvas.removeEventListener('mousemove', startDraw);
+});
 
 function changeCanvasSize() {
 	canvas.height = window.innerHeight;
@@ -18,48 +29,42 @@ function changeCanvasSize() {
 }
 changeCanvasSize();
 
-
-canvas.addEventListener('mousedown', () => {
-	paint  = true;
-});
-
-canvas.addEventListener('mouseup', () => {
-	paint  = false;
-});
-
-canvas.addEventListener('mouseleave', () => {
-	paint  = false;
-});
-
-canvas.addEventListener('mousemove', (e) => {
-	let posX = e.clientX;
-	let posY = e.clientY;
-	if (paint) {
-		if ( opts.radius === 100 ) {
-			max = true;
-			min = false;
-		} else if (opts.radius === 0 ) {
-			max = false;
-			min = true;
-		}
-
-		if ( max ) {
-			opts.radius--
-		} else if ( min ) {
-			opts.radius++;
-		}
+function startDraw(e) {
+	CalculateLineParametrs(e);
+	draw(e);
+}
 
 
-		if ( e.shiftKey ) {
-			currentHue === 0 ? currentHue = 359 : currentHue--;
-		} else {
-			currentHue === 359 ? currentHue = 0 : currentHue++;
-		}
-
-		ctx.fillStyle = opts.color.replace('hue', currentHue);
-		ctx.beginPath();
-		 ctx.arc(posX+5, posY+5, opts.radius, 0, Math.PI * 2);
-		ctx.lineTo(posX, posY);
-		ctx.fill();
+function CalculateLineParametrs(e) {
+	if ( opts.lineWidth === 100 ) {
+		max = true;
+		min = false;
+	} else if (opts.lineWidth === 5 ) {
+		max = false;
+		min = true;
 	}
-});
+
+	if ( max ) {
+		opts.lineWidth--
+	} else if ( min ) {
+		opts.lineWidth++;
+	}
+
+	if ( e.shiftKey ) {
+		currentHue === 0 ? currentHue = 359 : currentHue--;
+	} else {
+		currentHue === 359 ? currentHue = 0 : currentHue++;
+	}
+}
+
+function draw(e) {
+	let coordX = e.clientX;
+	let coordY = e.clientY;
+	ctx.beginPath();
+	ctx.strokeStyle = opts.color.replace('hue', currentHue);
+	ctx.lineJoin = 'round';
+	ctx.lineCap = 'round';
+	ctx.lineTo(coordX, coordY);
+	ctx.lineWidth = opts.lineWidth;
+	ctx.stroke();
+}
