7421d5e92a3b56e5b891669396ea67ff0f8e91ad
Nwke <adslk131007@mail.ru>
2017-08-19 19:53:39 +0300

    Add files via upload

---
+++ expandMenu/README.md
@@ -0,0 +1,2 @@
+# expandMenu
+expandMenu
---
+++ expandMenu/script.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Denis on 29.07.2017.
+ */
+
+let MainMenu = document.querySelectorAll('.wrapper-dropdown')[0];
+MainMenu.addEventListener('click', expandMenu);
+function expandMenu() {
+	MainMenu.classList.toggle('active')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
