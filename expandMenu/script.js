c2ebfc8f03d2761bf7228b5e2abf6a0c6b397855
Nwke <adslk131007@mail.ru>
2017-09-01 00:03:01 +0300

    Add files via upload

--- expandMenu/script.js
+++ expandMenu/script.js
@@ -1,40 +1,4 @@
-/**
- * Created by Denis on 29.07.2017.
- */
-
-let MainMenu = document.querySelectorAll('.wrapper-dropdown')[0];
-MainMenu.addEventListener('click', expandMenu);
-function expandMenu() {
-	MainMenu.classList.toggle('active')
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let myProfile = document.querySelector('.wrapper-dropdown');
+myProfile.addEventListener('click', () => {
+	myProfile.classList.toggle('active');
+});
