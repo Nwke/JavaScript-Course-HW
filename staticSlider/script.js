baca95b7f2b57dc0df47bcf3e4e1556a1ab34a08
Nwke <adslk131007@mail.ru>
2017-09-01 00:06:57 +0300

    Add files via upload

--- staticSlider/script.js
+++ staticSlider/script.js
@@ -1,73 +1,23 @@
-/**
- * Created by Denis on 29.07.2017.
- */
+let listImg = ['https://netology-code.github.io/hj-homeworks/browser/gallery/i/breuer-building.jpg',
+	'https://netology-code.github.io/hj-homeworks/browser/gallery/i/guggenheim-museum.jpg',
+	'https://netology-code.github.io/hj-homeworks/browser/gallery/i/headquarters.jpg',
+	'https://netology-code.github.io/hj-homeworks/browser/gallery/i/IAC.jpg',
+	'https://netology-code.github.io/hj-homeworks/browser/gallery/i/new-museum.jpg'];
 
-const sliderContent = document.querySelector('#currentPhoto');
+let nextPhoto = document.querySelector('#nextPhoto');
+let prevPhoto = document.querySelector('#prevPhoto');
+let currentPhoto = document.querySelector('#currentPhoto');
+let count = 0;
 
-const arr = ["https://netology-code.github.io/hj-homeworks/browser/gallery/i/breuer-building.jpg",
-							"https://netology-code.github.io/hj-homeworks/browser/gallery/i/guggenheim-museum.jpg",
-							"https://netology-code.github.io/hj-homeworks/browser/gallery/i/headquarters.jpg",
-							"https://netology-code.github.io/hj-homeworks/browser/gallery/i/IAC.jpg",
-							"https://netology-code.github.io/hj-homeworks/browser/gallery/i/new-museum.jpg"];
-let i = 0;
+nextPhoto.addEventListener('click', nextImg);
+prevPhoto.addEventListener('click', prevImg);
 
-const btnNext = document.querySelector('#nextPhoto');
-const btnPrev = document.querySelector('#prevPhoto');
-
-btnNext.addEventListener('click', updateImgSlider);
-btnPrev.addEventListener('click', updateImgSlider);
-
-function updateImgSlider(e) {
-	if (e.target === btnNext) {
-		if (i === arr.length-1) {
-			i = 0;
-			sliderContent.src = arr[i];
-		}
-		else {
-			i++;
-			sliderContent.src = arr[i];
-		}
-	}
-	else if (e.target === btnPrev) {
-		if (i === 0) {
-			i = 4;
-			sliderContent.src = arr[i];
-		}
-		else {
-			i--;
-			sliderContent.src = arr[i];
-		}
-	}
+function nextImg() {
+	count = count + 1 === listImg.length ? 0 : count + 1;
+	currentPhoto.src =  listImg[count];
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function prevImg() {
+	count = count - 1 === -1 ? listImg.length - 1 : count - 1;
+	currentPhoto.src = listImg[count];
+}
