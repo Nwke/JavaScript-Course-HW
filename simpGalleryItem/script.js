c2b7a552bc47470aec9f7807370788cdbf0d78a9
Nwke <adslk131007@mail.ru>
2017-08-31 23:42:57 +0300

    Add files via upload

--- simpGalleryItem/script.js
+++ simpGalleryItem/script.js
@@ -1,59 +1,18 @@
-/**
- * Created by Denis on 29.07.2017.
- */
+const listProduct = document.querySelector('#nav');
+const previewImgProduct = document.querySelector('#view');
+const nameCurrentProduct = document.querySelector('h2');
+// nameCurrentProduct я определил для вывода названия скейт-борда,
+// Но не уверен,что это надо,но думаю не помешает
+listProduct.addEventListener('click', selectedItem);
 
-const smolIMG = ['https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/01.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/02.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/03.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/04.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/05.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/05.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/06.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/thumb/07.jpg'];
-const bigIMG = ['https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/01.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/02.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/03.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/04.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/05.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/06.jpg','https://netology-code.github.io/hj-homeworks/event-object/skateboard-gallery/images/full/07.jpg'];
-var AllLinks = document.getElementsByTagName('a');
-const nameItem = document.querySelector('#title');
-var SuperiorIMG = document.getElementById('view');
-
-for (var tagA of AllLinks) {
-	tagA.addEventListener('click', current);
-	tagA.addEventListener('click', function(e) {
+function selectedItem(e) {
+	let elem = e.target;
+	if (elem.tagName === 'IMG' && elem.parentNode.tagName === 'A') {
 		e.preventDefault();
-	});
-}
-
-function current(e) {
-	var elem =  e.target;
-	for (var oneLink of AllLinks) {
-		oneLink.classList.remove('gallery-current');
+		let title = elem.title;
+		nameCurrentProduct.innerText = `Выбран Скейт-борд под названием:${title}`;
+		previewImgProduct.src = elem.src.replace(/thumb/, 'full');
+		document.querySelector('.gallery-current').classList.remove('gallery-current');
+		elem.parentNode.classList.add('gallery-current')
 	}
-
-	var nameSelected = elem.getAttribute('title');
-	nameItem.innerHTML = nameSelected;
-	elem.parentNode.classList.add('gallery-current');
-	var bestSrc = elem.parentNode.getAttribute('href');
-	SuperiorIMG.src = bestSrc;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
