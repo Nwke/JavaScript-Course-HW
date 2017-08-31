f046e4484a181c60cf4e4103f7f9cd40afe01181
Nwke <adslk131007@mail.ru>
2017-08-31 19:09:48 +0300

    Add files via upload

--- simplPlayer/script.js
+++ simplPlayer/script.js
@@ -1,107 +1,83 @@
-/**
- * Created by Denis on 29.07.2017.
- */
+// Список песен:
+// 1. LA Chill Tour,
+// https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Chill%20Tour.mp3
 
-const ArrSRC = ['https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Chill%20Tour.mp3',
+// 2. This is it band,
+// https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/This%20is%20it%20band.mp3
+
+// 3. LA Fusion Jam,
+// https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Fusion%20Jam.mp3
+
+
+const listTitleMelodies = ['LA Chill Tour', 'This is it band ', 'LA Fusion Jam'];
+const listMelodies = [
+	'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Chill%20Tour.mp3',
 	'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/This%20is%20it%20band.mp3',
-	'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Fusion%20Jam.mp3'];
-const ArrName = ['LA Chill Tour',
-	'This is it band',
-	'LA Fusion Jam'];
-let audioIndex = 0;
-let butPlay = document.getElementsByClassName('playstate')[0];
-let audioS = document.getElementsByTagName('audio')[0];
-let textMusic = document.getElementsByClassName('title')[0];
-let mediaPlayer = document.getElementsByClassName('mediaplayer')[0];
-let isPaused = false;
-butPlay.onclick = function () {
+	'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Fusion%20Jam.mp3'
+];
+const wrapperPlayer = document.querySelector('.mediaplayer');
+const titleMusics = document.querySelector('.title');
+const player = document.querySelector('audio');
+const changePlayerMode = document.querySelector('.playstate');
+const stop = document.querySelector('.stop');
+const next = document.querySelector('.next');
+const back = document.querySelector('.back');
+let countMusic = 0;
 
-	if (mediaPlayer.classList.contains('play')){
-		audioS.pause();
-		mediaPlayer.classList.remove('play');
-		isPaused = true;
+
+changePlayerMode.addEventListener('click', changeMode);
+stop.addEventListener('click', onStop);
+next.addEventListener('click', onNext);
+back.addEventListener('click', onBack);
+
+function changeMode() {
+	if (wrapperPlayer.classList.contains('play')) {
+		onPause()
+	} else {
+		onPlay();
 	}
-	else {
-		mediaPlayer.classList.add('play');
-		if (!isPaused) {
-			audioS.src = ArrSRC[audioIndex];
-			textMusic.title = ArrName[audioIndex];
-		}
-		audioS.play();
-		isPaused = false;
-	}
-};
-var stop = document.getElementsByClassName('stop')[0];
-stop.onclick = function stopS() {
-	mediaPlayer.classList.remove('play');
-	textMusic.title = 'OFF';
-	audioS.pause();
-	audioS.currentTime = 0;
-	isPaused = false;
-};
-var back = document.getElementsByClassName('back')[0];
-back.onclick = function () {
-	if ( audioIndex >= 1){
-		audioS.pause();
-		audioIndex -=1;
-		mediaPlayer.classList.add('play');
-		audioS.src = ArrSRC[audioIndex];
-		textMusic.title = ArrName[audioIndex];
-		audioS.play();
-	}
-	else {
-		audioIndex = 2;
-		mediaPlayer.classList.add('play');
-		audioS.pause();
-		audioS.src = ArrSRC[audioIndex];
-		textMusic.title = ArrName[audioIndex];
-		audioS.play();
-	}
-};
-var next = document.getElementsByClassName('next')[0];
-next.onclick = function () {
-	if( audioIndex === 2 ){
-		audioS.pause();
-		audioIndex = 0;
-		mediaPlayer.classList.add('play');
-		audioS.src = ArrSRC[audioIndex];
-		textMusic.title = ArrName[audioIndex];
-		audioS.play();
+}
 
-	}
-	else {
-		mediaPlayer.classList.add('play');
-		audioS.pause();
-		audioIndex +=1;
-		audioS.src = ArrSRC[audioIndex];
-		textMusic.title = ArrName[audioIndex];
-		audioS.play();
+function changeTitleMelodies() {
+	titleMusics.title = listTitleMelodies[countMusic];
+}
 
-	}
-};
+function onPlay() {
+	changeTitleMelodies();
+	player.play();
+	wrapperPlayer.classList.add('play');
+}
 
+function onPause() {
+	changeTitleMelodies();
+	player.pause();
+	wrapperPlayer.classList.remove('play');
+}
 
+function onStop() {
+	onPause();
+	player.currentTime = 0;
+}
 
+function onNext() {
+	let instReproduce = false;
+	if (wrapperPlayer.classList.contains('play')) instReproduce = true;
+	countMusic = countMusic === 2 ? 0 : countMusic + 1;
+	onStop();
+	changeTitleMelodies();
+	player.src = listMelodies[countMusic];
+	if ( instReproduce) onPlay();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function onBack() {
+	let instReproduce = false;
+	if (wrapperPlayer.classList.contains('play')) instReproduce = true;
+	countMusic = countMusic === 0 ? 2 : countMusic - 1;
+	onStop();
+	changeTitleMelodies();
+	player.src = listMelodies[countMusic];
+	if ( instReproduce) onPlay();
+}
 
 
 
