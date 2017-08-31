d96ea3e2abcbeff17bf68f9b2690fe4775aa55af
Nwke <adslk131007@mail.ru>
2017-08-31 23:59:16 +0300

    Add files via upload

--- KeyboardEvents/script.js
+++ KeyboardEvents/script.js
@@ -1,57 +1,23 @@
-/**
- * Created by Denis on 29.07.2017.
- */
+(function () {
+	document.addEventListener('keydown', openHiddenMenu);
+	document.addEventListener('keydown', openSecretBlock);
+	let countSecretCode = 0;
 
-document.addEventListener('keydown',ready);
-document.addEventListener('keydown',openSecret);
-function ready(e) {
-	const nav = document.getElementsByTagName('nav')[0];
-	if ((e.ctrlKey) && (e.altKey) && (e.keyCode === 84)) {
-		nav.classList.toggle("visible");
-	}
-}
-const code = [89,84,78,74,75,74,85,66,90];
-var i =0;
-var win = false;
-function openSecret(e) {
-	if ( e.keyCode === code[i] ) {
-		i++;
-		if (i === code.length) {
-			var secret = document.getElementsByClassName("secret")[0];
-			secret.classList.add("visible");
+	function openHiddenMenu(e) {
+		if (e.ctrlKey && e.altKey && e.keyCode === 84) {
+			document.querySelector('nav').classList.toggle('visible');
 		}
 	}
-	else {
-		i= 0;
+
+	function openSecretBlock(e) {
+		const secretCode = [89, 84, 78, 74, 75, 74, 85, 66, 90];
+		if (e.keyCode === secretCode[countSecretCode]) {
+			countSecretCode++;
+			if (countSecretCode === secretCode.length ) {
+				document.querySelector('.secret').classList.add('visible');
+			}
+		} else {
+			countSecretCode = 0;
+		}
 	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})();
