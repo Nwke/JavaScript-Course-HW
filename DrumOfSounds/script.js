c97ff7ce3ecf275b6e29ee5ea527f9d4e3315fec
Nwke <adslk131007@mail.ru>
2017-09-01 00:04:15 +0300

    Add files via upload

--- DrumOfSounds/script.js
+++ DrumOfSounds/script.js
@@ -1,44 +1,7 @@
-/**
- * Created by Denis on 29.07.2017.
- */
-
-let listAudio = document.querySelectorAll('.drum-kit__drum');
-
-for (let i = 0; i < listAudio.length; i++){
-	listAudio[i].addEventListener('click', activedSound)
-}
-function activedSound(e) {
-	let sound =  e.target.lastChild;
-	sound.play();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Ссылка,которая в аттрибуте src у тега audio элемента с классом key-openhat невалидная,поэтому заменил.
+document.querySelector('.key-openhat').querySelector('audio').src = 'https://netology-code.github.io/hj-homeworks/html-element-collection/drum-machine/wav/openhat.wav';
+document.querySelector('.drum-kit').addEventListener('click', (e) => {
+	if (e.target.querySelector('audio')) {
+		e.target.querySelector('audio').play();
+	}
+});
